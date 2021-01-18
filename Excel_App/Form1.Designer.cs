035d1b70b225cf6bba4d4733a1fcaea96863df78
Пользователь <@DESKTOP-3U38BLH>
2021-01-18 16:35:37 +0200

    Add project files.

---
+++ Excel_App/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Excel_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Import = new System.Windows.Forms.Button();
+            this.btn_Export = new System.Windows.Forms.Button();
+            this.btn_SumPrice = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(709, 348);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_Import
+            // 
+            this.btn_Import.Location = new System.Drawing.Point(23, 395);
+            this.btn_Import.Name = "btn_Import";
+            this.btn_Import.Size = new System.Drawing.Size(113, 43);
+            this.btn_Import.TabIndex = 1;
+            this.btn_Import.Text = "Import";
+            this.btn_Import.UseVisualStyleBackColor = true;
+            this.btn_Import.Click += new System.EventHandler(this.btn_Import_Click);
+            // 
+            // btn_Export
+            // 
+            this.btn_Export.Location = new System.Drawing.Point(561, 395);
+            this.btn_Export.Name = "btn_Export";
+            this.btn_Export.Size = new System.Drawing.Size(113, 43);
+            this.btn_Export.TabIndex = 2;
+            this.btn_Export.Text = "Export";
+            this.btn_Export.UseVisualStyleBackColor = true;
+            this.btn_Export.Click += new System.EventHandler(this.btn_Export_Click);
+            // 
+            // btn_SumPrice
+            // 
+            this.btn_SumPrice.Location = new System.Drawing.Point(210, 404);
+            this.btn_SumPrice.Name = "btn_SumPrice";
+            this.btn_SumPrice.Size = new System.Drawing.Size(75, 24);
+            this.btn_SumPrice.TabIndex = 3;
+            this.btn_SumPrice.Text = "Sum";
+            this.btn_SumPrice.UseVisualStyleBackColor = true;
+            this.btn_SumPrice.Click += new System.EventHandler(this.btn_SumPrice_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(305, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_SumPrice);
+            this.Controls.Add(this.btn_Export);
+            this.Controls.Add(this.btn_Import);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Import;
+        private System.Windows.Forms.Button btn_Export;
+        private System.Windows.Forms.Button btn_SumPrice;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Excel_App/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Excel_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable table = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Processor Type", typeof(string));
+            table.Columns.Add("Memory RAM", typeof(string));
+            table.Columns.Add("Price", typeof(int));
+
+            dataGridView1.DataSource = table;
+
+        }
+        private void btn_Import_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\Пользователь\source\repos\Excel_App\FileExcel.txt");
+            string[] values;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split('/');
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+
+                }
+                table.Rows.Add(row);
+            }
+        }
+
+        private void btn_Export_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "AboutLaptops";
+            
+             for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+             {
+                 worksheet.Cells[i, 1] = dataGridView1.Columns[i - 1].HeaderText;
+             }
+
+             for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+             {
+                 for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                 {
+                     worksheet.Cells[i + 6, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                 }
+             }
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "output";
+            saveFileDialog.DefaultExt = ".xlsx";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                workbook.SaveAs(saveFileDialog.FileName, Type.Missing,Type.Missing,Type.Missing,Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            }
+            app.Quit();
+        }
+
+        private void btn_SumPrice_Click(object sender, EventArgs e)
+        {
+            int[] columnData = new int[dataGridView1.Rows.Count];
+            columnData = (from DataGridViewRow row in dataGridView1.Rows
+                          where row.Cells[4].FormattedValue.ToString() != string.Empty
+                          select Convert.ToInt32(row.Cells[4].FormattedValue)).ToArray();
+            label1.Text = columnData.Sum().ToString();
+
+        }
+    }
+}
